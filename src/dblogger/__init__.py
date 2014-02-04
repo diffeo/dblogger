f45a4ca0bd42ee27e5e512627005b7586ab0be0c
David Maze <dmaze@diffeo.com>
2014-02-04 21:40:04 +0000

    export dblogger.configure_logging (from configure)

--- src/dblogger/__init__.py
+++ src/dblogger/__init__.py
@@ -1,5 +1,7 @@
 
-from logger import *
-from query import *
-from format import *
+from __future__ import absolute_import
+from dblogger.configure import configure_logging
+from dblogger.logger import *
+from dblogger.query import *
+from dblogger.format import *
 
